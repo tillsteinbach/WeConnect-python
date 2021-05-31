6effc5d4152a30b5d402ebd729e250734dfd78c4
steinbach <till.steinbach@informatik.haw-hamburg.de>
2021-05-31 07:56:01 +0200

    first set of tests

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,4 +1,27 @@
+import os
+import json
+
+import pytest
+
+from weconnect import elements
+
+SCRIPTLOC = os.path.dirname(__file__)
 
 
-def test_dummy():
-    assert True
+@pytest.mark.parametrize('className, testcase', [(elements.ClimatizationTimer, 'complete')])
+def test_Elements(request, className, testcase):
+    with open(f'{request.config.rootdir}/tests/ressources/elements/{className.__name__}/{testcase}.json') as file:
+        dict = json.load(file)
+        element = className(parent=None, statusId='test', fromDict=dict)
+
+        element2 = className(parent=None, statusId='test', fromDict=None)
+        element2.update(dict)
+        print(repr(str(element2)))
+
+        assert element is not None
+
+        with open(f'{request.config.rootdir}/tests/ressources/elements/{className.__name__}/{testcase}.resultstr') \
+                as resultStringFile:
+            expectedResult = resultStringFile.read()
+
+            assert expectedResult == str(element2)
