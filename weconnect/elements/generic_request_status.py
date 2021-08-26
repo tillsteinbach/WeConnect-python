ed947ca04831bdf08cb486900c60adde650918ac
Till Steinbach <steinbach.till@gmx.de>
2021-08-26 23:02:40 +0200

    Added new error state delayed

--- weconnect/elements/generic_request_status.py
+++ weconnect/elements/generic_request_status.py
@@ -70,5 +70,4 @@ class GenericRequestStatus(GenericStatus):
         TIMEOUT = 'timeout'
         FAIL_VEHICLE_IS_OFFLINE = 'fail_vehicle_is_offline'
         FAIL_IGNITION_ON = 'fail_ignition_on'
-        DELAYED = 'delayed'
         UNKNOWN = 'unknown status'
--- weconnect/elements/generic_status.py
+++ weconnect/elements/generic_status.py
@@ -176,7 +176,6 @@ class GenericStatus(AddressableObject):
             TIMEOUT = 'timeout'
             FAIL_VEHICLE_IS_OFFLINE = 'fail_vehicle_is_offline'
             FAIL_IGNITION_ON = 'fail_ignition_on'
-            DELAYED = 'delayed'
             UNKNOWN = 'unknown status'
 
     class StatusError(AddressableObject):
