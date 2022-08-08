a67e6b07ff08d094859853d3eacdcb48a3c5d3e4
Till Steinbach <steinbach.till@gmx.de>
2022-08-08 22:22:38 +0200

    typing

--- weconnect/weconnect.py
+++ weconnect/weconnect.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Dict, List, Set, Tuple, Callable, Any, Optional
+from typing import Dict, List, Set, Tuple, Callable, Any, Optional, Union
 
 import os
 import string
@@ -29,7 +29,7 @@ class WeConnect(AddressableObject):  # pylint: disable=too-many-instance-attribu
         self,
         username: str,
         password: str,
-        spin: str = None,
+        spin: Union[str,bool] = None,
         tokenfile: Optional[str] = None,
         updateAfterLogin: bool = True,
         loginOnInit: bool = False,
@@ -67,7 +67,7 @@ class WeConnect(AddressableObject):  # pylint: disable=too-many-instance-attribu
         super().__init__(localAddress='', parent=None)
         self.username: str = username
         self.password: str = password
-        self.spin: str = spin
+        self.spin: Union[str,bool] = spin
 
         self.__userId: Optional[str] = None  # pylint: disable=unused-private-member
         self.__session: requests.Session = requests.Session()
