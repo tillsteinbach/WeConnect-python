cfea3da359230756ca6bae818964674e19d326c0
bendavidson <ginge6000@hotmail.com>
2023-10-27 08:26:12 +0100

    Ignore range errors caused by VW mile/km conversion (#155)
    Co-authored-by: Ben Davidson <ben.davidson@turners-distribution.com>

--- weconnect/elements/battery_status.py
+++ weconnect/elements/battery_status.py
@@ -26,18 +26,25 @@ class BatteryStatus(GenericStatus):
         LOG.debug('Update battery status from dict')
 
         if 'value' in fromDict:
-            self.currentSOC_pct.fromDict(fromDict['value'], 'currentSOC_pct')
-
             if 'cruisingRangeElectric_km' in fromDict['value']:
                 cruisingRangeElectric_km = int(fromDict['value']['cruisingRangeElectric_km'])
                 if self.fixAPI and cruisingRangeElectric_km == 0x3FFF:
                     cruisingRangeElectric_km = None
                     LOG.info('%s: Attribute cruisingRangeElectric_km was error value 0x3FFF. Setting error state instead'
                              ' of 16383 km.', self.getGlobalAddress())
-                self.cruisingRangeElectric_km.setValueWithCarTime(
-                    cruisingRangeElectric_km, lastUpdateFromCar=None, fromServer=True)
+
+                if (self.fixAPI
+                    and round((self.cruisingRangeElectric_km.value or 0) * 0.621371) == cruisingRangeElectric_km
+                    and self.currentSOC_pct.value == int(fromDict['value']['currentSOC_pct'])):
+                    LOG.info('%s: Attribute cruisingRangeElectric_km was miscalculated (miles/km) this is a bug in the API and the new value will not be used',
+                             self.getGlobalAddress())
+                else:
+                    self.cruisingRangeElectric_km.setValueWithCarTime(
+                        cruisingRangeElectric_km, lastUpdateFromCar=None, fromServer=True)
             else:
                 self.cruisingRangeElectric_km.enabled = False
+
+            self.currentSOC_pct.fromDict(fromDict['value'], 'currentSOC_pct')
         else:
             self.currentSOC_pct.enabled = False
             self.cruisingRangeElectric_km.enabled = False
